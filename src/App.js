d036c52a38bd9d7339218f8e6b59a0b6011d182b
Michelle Burton <michelle_burton@bradycorp.com>
2019-09-16 10:51:27 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Button from './components/Button';
+import styled from 'styled-components';
+
+const Button2 = styled.button`
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  border-radius: 3px;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+  &:hover {
+    background-color: palevioletred;
+    color: white;
+  }
+`;
 
 function App() {
   return (
@@ -18,7 +37,10 @@ function App() {
         >
           Learn React
         </a>
+        <Footer></Footer>
+        <Button2>Styled Button</Button2>
       </header>
+
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Button extends React.Component {
+    render() {
+        return (
+            <div>xx</div>
+        );
+    }
+}
+
+export default Button;
